3a33c4932191efbe2987df08278455d29bb857f4
Jacinto Mendes <jmz093@gmail.com>
2021-02-01 17:42:33 +1100

    Added bootstrap CDN css ; created store, reducer for 1 field ; TODO validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import {Step1 } from "./components/Step1"
+import {Result} from "./components/Result"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path="/" component={Step1} /> 
+      <Route path="/result" component={Result} /> 
+    </Router>
+  )
 }
 
 export default App;
